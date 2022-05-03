63d7b4d09dc2f42f369fbd3d547f6dc282522968
aribonfil <ariel.bonfil@corebiz.ag>
2022-05-03 13:02:01 -0300

    input file and convert to json

--- react/IndexCatalogueManager.jsx
+++ react/IndexCatalogueManager.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
+import * as XLSX from "xlsx";
 const IndexManager = () => {
+  const readUploadFile = (e) => {
+    e.preventDefault();
+    if (e.target.files) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            const data = e.target.result;
+            const workbook = XLSX.read(data, { type: "array" });
+            const sheetName = workbook.SheetNames[0];
+            const worksheet = workbook.Sheets[sheetName];
+            const json = XLSX.utils.sheet_to_json(worksheet);
+            console.log(json);
+        };
+        reader.readAsArrayBuffer(e.target.files[0]);
+    }
+}
 
   return (
-
- <React.Fragment>
- abc
-
-</React.Fragment>
-  )
+<form>
+    <label htmlFor="upload">Upload File</label>
+    <input
+        type="file"
+        name="upload"
+        id="upload"
+        onChange={readUploadFile}
+    />
+</form>
+)
 }
 export default IndexManager
