2e7917913088bd15d3f16290f8292517e604ae1a
aribonfil <ariel.bonfil@corebiz.ag>
2022-05-04 17:15:19 -0300

    input file and convert to json and handle data to send post

--- react/IndexCatalogueManager.jsx
+++ react/IndexCatalogueManager.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as XLSX from "xlsx";
-const IndexManager = () => {
-  const readUploadFile = (e) => {
+import { sendData } from "./components/actions"
+const index = () => {
+
+    const [data, setData] = useState()
+    const [dataImages, setDataImages] = useState()
+
+    const readfileData = (e) => {
     e.preventDefault();
     if (e.target.files) {
         const reader = new FileReader();
@@ -11,22 +16,53 @@ const IndexManager = () => {
             const sheetName = workbook.SheetNames[0];
             const worksheet = workbook.Sheets[sheetName];
             const json = XLSX.utils.sheet_to_json(worksheet);
-            console.log(json);
+            setData(json)
+
         };
         reader.readAsArrayBuffer(e.target.files[0]);
     }
 }
+const readfileImages = (e) => {
+  e.preventDefault();
+  if (e.target.files) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+          const data = e.target.result;
+          const workbook = XLSX.read(data, { type: "array" });
+          const sheetName = workbook.SheetNames[0];
+          const worksheet = workbook.Sheets[sheetName];
+          const json = XLSX.utils.sheet_to_json(worksheet);
+          setDataImages(json)
 
+      };
+      reader.readAsArrayBuffer(e.target.files[0]);
+  }
+}
+  console.log("asddsada", data, dataImages);
   return (
+    <div>
 <form>
-    <label htmlFor="upload">Upload File</label>
+    <label htmlFor="upload">Upload File Data</label>
     <input
         type="file"
-        name="upload"
-        id="upload"
-        onChange={readUploadFile}
+        name="fileData"
+        id="fileData"
+        onChange={readfileData}
+    />
+      <label htmlFor="upload">Upload File Images</label>
+    <input
+        type="file"
+        name="fileImages"
+        id="fileImages"
+        onChange={readfileImages}
     />
 </form>
+{data && dataImages &&
+<button onClick={() => sendData(data, dataImages)}>
+  Cargar
+</button>
+}
+</div>
 )
 }
-export default IndexManager
+export default index
---
+++ react/components/actions.js
@@ -0,0 +1,8 @@
+
+export const sendData = async (data, dataImages) => {
+  // TODO: Hacer un post enviando los datos
+  console.log(data, dataImages);
+  return true;
+}
+
+
